69dd0801a30c1982dc5b997820b89e1cf89bb4d0
Menno van Slooten <menno.vanslooten@gmail.com>
2014-01-31 17:49:17 +0100

    Some viewport/mouse refactoring. New feature: specify widthxheight for open action

--- self_test/assets/mouse.js
+++ self_test/assets/mouse.js
@@ -1,17 +1,30 @@
 (function($) {
+	var $win = $(window);
+	var $doc = $(document);
+	console.log('window initial:', $win.width(), $win.height());
+
+
 	$('.mouse_target').on('click mouseenter mouseleave dblclick', function(e) {
 		console.log(this.className, ' - ' , e.type);
 		$(this).addClass(e.type);
 	});
 
 
-	$(document).on('mousemove', function(e) {
+	$doc.on('mousemove', function(e) {
 		var $crumb = $('<div class="mouse_trail"/>');
 		$crumb.appendTo('body');
 		$crumb.css({
 			left: e.pageX + 1,
 			top: e.pageY + 1
 		});
+	});
+
+	$doc.on('scroll', function(e) {
+		console.log('scrolling', $doc.scrollTop());
+	});
+
+	$win.on('resize', function(e) {
+		console.log('window resize:', $win.width(), $win.height());
 	});
 
 })(jQuery, undefined);
