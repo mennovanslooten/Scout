aeb2cb56c3a5a5a74e14f152256372e838d2f8d7
Menno van Slooten <menno.vanslooten@gmail.com>
2014-10-31 10:11:43 +0100

    Added some comments

--- dummy.js
+++ dummy.js
@@ -10,6 +10,7 @@ var _cli_args           = require('./lib/arguments').parseArguments();
 var _testrunner         = require('./lib/testrunner');
 var _parser             = require('./lib/testparser');
 var _logger             = require('./lib/logger');
+var _xunit              = require('./lib/xunit');
 var _remembered         = {};
 
 var _suite = {
@@ -25,6 +26,9 @@ function Dummy() {
 	var _test_index = -1;
 	var _running    = 0;
 
+	/**
+	 * Run the next test from _suite.tests
+	 */
 	function nextTest() {
 		// Maximum parallel tests running
 		if (_running >= _cli_args.parallel) return;
@@ -45,26 +49,36 @@ function Dummy() {
 	}
 
 
+	/**
+	 * Register a test as passed
+	 */
 	function passTest(test_data) {
 		_running--;
 		test_data.end_time = new Date();
 		_suite.passed.push(test_data);
 
-		_logger.passTest(test_data, _suite);
+		//_logger.passTest(test_data, _suite);
 		checkDone();
 	}
 
 
+	/**
+	 * Register a test as failed
+	 */
 	function failTest(test_data) {
 		_running--;
 		test_data.end_time = new Date();
 		_suite.failed.push(test_data);
 
-		_logger.failTest(test_data, _suite);
+		//_logger.failTest(test_data, _suite);
 		checkDone();
 	}
 
 
+	/**
+	 * If all test are either passed or failed we are done, otherwise run the
+	 * next test
+	 */
 	function checkDone() {
 		if (_suite.passed.length + _suite.failed.length === _suite.tests.length) {
 			return done();
@@ -74,14 +88,19 @@ function Dummy() {
 	}
 
 
+	/**
+	 * Done testing. Log and exit.
+	 */
 	function done() {
 		_suite.end_time = new Date();
 		_logger.done(_suite);
+		//_xunit.log(_suite);
 		var is_passed = _suite.failed.length === 0;
 		var exit_code = is_passed ? 0 : 1;
 		return phantom.exit(exit_code);
 	}
 
+	// Get the party started
 	nextTest()
 };
 
