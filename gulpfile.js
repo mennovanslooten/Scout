df9775a6609d08ae5d391cd8267accb46122a392
Menno van Slooten <menno.vanslooten@gmail.com>
2015-03-06 10:56:40 +0100

    Added scout self_tests to gulp and npm

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var spawn = require('child_process').spawn;
 
-var lib_js_files = ['./scout.js', './lib/**/*.js'];
+var scout_files = ['./self_tests/**/*.scout'];
+var lib_js_files = ['./index.js', './lib/**/*.js'];
 var test_js_files = ['./test/**/*.js'];
 var all_js_files = lib_js_files.concat(test_js_files);
 
@@ -19,12 +21,13 @@ gulp.task('default', ['test']);
 
 gulp.task('watch', ['default'], function() {
 	gulp.watch('./.jscsrc', ['jscs']);
+	gulp.watch(scout_files, ['scout']);
 	gulp.watch(all_js_files, ['test']);
 	gulp.watch(test_js_files, ['mocha']);
 });
 
 
-gulp.task('test', ['jshint', 'jscs', 'mocha']);
+gulp.task('test', ['jshint', 'jscs', 'mocha', 'scout']);
 
 
 gulp.task('mocha', function(cb) {
@@ -36,7 +39,8 @@ gulp.task('mocha', function(cb) {
 gulp.task('jshint', function() {
 	return gulp.src(all_js_files)
 		.pipe(jshint('.jshintrc'))
-		.pipe(jshint.reporter('default'));
+		.pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'));
 });
 
 
@@ -46,4 +50,27 @@ gulp.task('jscs', function() {
 });
 
 
+gulp.task('scout', function(done) {
+    var scout = spawn('./bin/scout', ['self_tests', '--parallel=3', '--timeout=1000']);
+    scout.stdout.setEncoding('utf8');
 
+    var log = '';
+    scout.stdout.on('data', function(data) {
+        var str = data.toString()
+        log += str;
+    });
+
+    //scout.stdout.on('end', function(data) {
+        //done();
+    //});
+
+
+    scout.on('close', function (code) {
+        if (code) {
+            console.log(log);
+            done(new Error());
+        } else {
+            done();
+        }
+    });
+});
