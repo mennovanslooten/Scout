a8c323fa87a478cee5d5a6b1dda9d15bfacde65a
Menno van Slooten <menno.vanslooten@gmail.com>
2015-04-20 16:17:56 +0200

    Cleaned up xunit.js

--- src/logger/xunit.js
+++ src/logger/xunit.js
@@ -11,53 +11,69 @@ function getDuration(obj) {
 }
 
 
-exports.write = function(filename, suite) {
+function getTestCaseXML(action, test) {
+    var type = action.type;
+    var duration = getDuration(action);
+    var xml = '\n    <testcase classname="' + type + '" time="' + duration + '">';
+
+    if (action.message) {
+        var failure_type = test.skipped.indexOf(action) > -1 ? 'error' : 'failure';
+        xml += '\n      <' + failure_type + ' type="' + type + '">';
+        xml += '<![CDATA[' + action.message + ']]>';
+        xml += '</' + failure_type + '>\n    ';
+    }
+
+    xml += '</testcase>';
+    return xml;
+}
+
+
+function getTestSuiteXML(test) {
+    var timestamp = test.start_time.toISOString();
+    var duration = getDuration(test);
+    var last_index = test.path.lastIndexOf(_fs.separator);
+    var pkg = test.path.substr(0, last_index);
+    var name = test.path.substr(last_index + 1);
+    var actions = test.actions.length;
+    var passed = test.passed.length;
+    var skipped = test.skipped.length;
+    var failed = passed + skipped === actions ? 0 : 1;
+    var total = passed + skipped + failed;
+
+    var xml = '\n  <testsuite errors="' + (failed + skipped) + '" failures="' + failed + '" ';
+    xml += 'name="' + name + '" package="' + pkg + '" tests="' + total + '" ';
+    xml += 'time="' + duration + '" timestamp="' + timestamp + '">';
+
+    for (var ii = 0; ii < total; ii++) {
+        var action = test.actions[ii];
+        xml += getTestCaseXML(action, test);
+    }
+
+    xml += '\n    <system-out/>';
+    xml += '\n    <system-err/>';
+    xml += '\n  </testsuite>';
+
+    return xml;
+}
+
+
+function getXML(suite) {
     var xml = '<?xml version="1.0" encoding="UTF-8"?>';
     var duration = getDuration(suite);
     xml += '\n<testsuites time="' + duration + '">';
 
     for (var i = 0; i < suite.tests.length; i++) {
         var test = suite.tests[i];
-        var timestamp = test.start_time.toISOString();
-        duration = getDuration(test);
-        var last_index = test.path.lastIndexOf(_fs.separator);
-        var pkg = test.path.substr(0, last_index);
-        var name = test.path.substr(last_index + 1);
-        var actions = test.actions.length;
-        var passed = test.passed.length;
-        var skipped = test.skipped.length;
-        var failed = passed + skipped === actions ? 0 : 1;
-        var total = passed + skipped + failed;
-
-        xml += '\n  <testsuite errors="' + (failed + skipped) + '" failures="' + failed + '" ';
-        xml += 'name="' + name + '" package="' + pkg + '" tests="' + total + '" ';
-        xml += 'time="' + duration + '" timestamp="' + timestamp + '">';
-
-        for (var ii = 0; ii < total; ii++) {
-            var action = test.actions[ii];
-            var type = action.type;
-            duration = getDuration(action);
-
-            xml += '\n    <testcase classname="' + type + '" time="' + duration + '">';
-
-            if (action.message) {
-                var failure_type = test.skipped.indexOf(action) > -1 ? 'error' : 'failure';
-                xml += '\n      <' + failure_type + ' type="' + type + '">';
-                xml += '<![CDATA[' + action.message + ']]>';
-                xml += '</' + failure_type + '>\n    ';
-            }
-
-            xml += '</testcase>';
-        }
-        xml += '\n    <system-out/>';
-        xml += '\n    <system-err/>';
-        xml += '\n  </testsuite>';
+        xml += getTestSuiteXML(test);
     }
+
     xml += '\n</testsuites>\n';
+    return xml;
+}
 
 
+exports.write = function(filename, suite) {
+    var xml = getXML(suite);
     _fs.write(filename, xml, 'w');
 };
-
-
 
