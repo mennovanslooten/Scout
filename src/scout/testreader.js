3c8c93c4ccbb6475d308ff5a44486c306befb448
Menno van Slooten <menno.vanslooten@gmail.com>
2015-12-09 11:33:43 +0100

    Added user_test property to action data

--- src/scout/testreader.js
+++ src/scout/testreader.js
@@ -11,7 +11,8 @@ var _cli = require('../utils/cli');
 var separator_rx = / {2,}| *\t+ */g;
 
 function parseLine(line, line_nr, path) {
-    var result = {
+    var user_action_types = ['include', 'click', 'moveMouseTo', 'type', 'open', 'uploadFile', 'back', 'forward'];
+    var action_data = {
         type: '',
         args: [],
         parts: [],
@@ -37,29 +38,32 @@ function parseLine(line, line_nr, path) {
     if (ignore.test(line)) {
         return null;
     } else if (log.test(line)) {
-        result.type = 'log';
-        result.args.push(line.match(log)[1]);
+        action_data.type = 'log';
+        action_data.args.push(line.match(log)[1]);
     } else {
         var parts = line.split(separator_rx);
-        result.parts = parts.concat();
-        result.type = parts.shift();
+        action_data.parts = parts.concat();
+        action_data.type = parts.shift();
 
         // An action starting with "?" is optional
         var optional = /^\?\s*(.+)/;
 
-        if (result.type.indexOf('@') === 0) {
-            var setting = result.type.substr(1);
+        if (action_data.type.indexOf('@') === 0) {
+            var setting = action_data.type.substr(1);
             parts.unshift(setting);
-            result.type = 'set';
-        } else if (optional.test(result.type)) {
-            result.type = result.type.match(optional)[1];
-            result.optional = true;
+            action_data.type = 'set';
+        } else if (optional.test(action_data.type)) {
+            action_data.type = action_data.type.match(optional)[1];
+            action_data.optional = true;
         }
 
-        result.args = parts;
+        action_data.args = parts;
     }
 
-    return result;
+    var is_user_action = user_action_types.indexOf(action_data.type) > -1;
+    action_data.user_action = is_user_action;
+
+    return action_data;
 }
 
 
