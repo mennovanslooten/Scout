ea642572aba4150fa2b36f37b38ca4c48e03a119
Menno van Slooten <menno.vanslooten@gmail.com>
2015-12-19 09:54:47 +0100

    More refactoring: removed fail/skip/pass callback logic in favor of generic complete

--- src/scout/testrunner.js
+++ src/scout/testrunner.js
@@ -2,6 +2,7 @@
 
 var _logger = require('../logger/logger');
 var _cli = require('../utils/cli');
+var _db         = require('./db');
 
 
 /**
@@ -9,7 +10,7 @@ var _cli = require('../utils/cli');
  * actions are succesfully completed, otherwise run failCallback after the
  * first failed action.
  */
-exports.run = function(test_data, passCallback, failCallback) {
+exports.run = function(test_data, completeCallback) {
 
     // testcontainer is the context in which a test is run, webpage, mouse,
     // keyboard, etc
@@ -28,7 +29,29 @@ exports.run = function(test_data, passCallback, failCallback) {
         if (action_index >= test_data.actions.length) return done();
 
         var action_data = test_data.actions[action_index];
-        testcontainer.runAction(action_data, passAction, skipAction, failAction);
+        // testcontainer.runAction(action_data, passAction, skipAction, failAction);
+        testcontainer.runAction(action_data, completeAction);
+    }
+
+
+    /**
+     * Register an action as completed
+     */
+    function completeAction(action_data) {
+        action_data.end_time = new Date();
+        _logger.logAction(action_data, test_data);
+
+        if (_db.isFailedAction(action_data)) {
+            testcontainer.failDump(action_data);
+            testcontainer.close();
+            completeCallback(test_data);
+        } else if (_cli.compare && action_data.user_action) {
+            testcontainer.passDump(action_data);
+            testcontainer.compareActionResult(action_data, passAction, skipAction);
+        } else {
+            testcontainer.passDump(action_data);
+            nextAction();
+        }
     }
 
 
@@ -39,7 +62,7 @@ exports.run = function(test_data, passCallback, failCallback) {
         action_data.end_time = new Date();
         _logger.passAction(action_data, test_data);
         testcontainer.passDump(action_data);
-        test_data.passed.push(action_data);
+        // test_data.passed.push(action_data);
 
         if (_cli.compare && action_data.user_action) {
             testcontainer.compareActionResult(action_data, passAction, skipAction);
@@ -55,7 +78,6 @@ exports.run = function(test_data, passCallback, failCallback) {
     function skipAction(action_data) {
         action_data.end_time = new Date();
         _logger.skipAction(action_data, test_data);
-        // test_data.skipped.push(action_data);
         nextAction();
     }
 
@@ -67,9 +89,8 @@ exports.run = function(test_data, passCallback, failCallback) {
         action_data.end_time = new Date();
         _logger.failAction(action_data, test_data);
         testcontainer.failDump(action_data);
-        // test_data.failed.push(action_data);
         testcontainer.close();
-        failCallback(test_data);
+        completeCallback(test_data);
     }
 
 
@@ -78,7 +99,7 @@ exports.run = function(test_data, passCallback, failCallback) {
      */
     function done() {
         testcontainer.close();
-        passCallback(test_data);
+        completeCallback(test_data);
     }
 
     // Kick off the first action
